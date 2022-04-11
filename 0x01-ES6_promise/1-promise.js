eebeb24675b803b65dca1be8cca3726b54aafbd9
= <=>
2022-04-12 00:10:33 +0100

    res rej

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
-export default function getResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return Promise((resolve, reject) => {
     if (success) {
       resolve({
