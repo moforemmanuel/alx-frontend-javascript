055dcec4f372f7ab3637981efcdf9cd0f9a02655
= <=>
2022-04-12 22:46:03 +0100

    startswith mod

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,5 @@
 export default function cleanSet(set, startString) {
-  return Array.from(set).filter((entry) => entry.startsWith(startString));
+  return '-'.join(Array.from(set)
+    .filter((entry) => entry.startsWith(startString))
+    .map((item) => item.replace(startString), ''));
 }
