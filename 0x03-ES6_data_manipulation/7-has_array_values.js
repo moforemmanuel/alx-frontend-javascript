d8530e2e33e0296e6d4af0999fa2ec457011ffbd
= <=>
2022-04-12 22:33:27 +0100

    set has array

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,8 +1,14 @@
 export default function hasValuesFromArray(set, array) {
   // eslint-disable-next-line consistent-return
-  array.forEach((item) => {
-    if (!set.has(item)) return false;
-  });
+  // array.forEach((item) => {
+  //   if (!set.has(item)) return false;
+  // });
+
+  for (const item of array) {
+    if (!set.has(item)) {
+      return false;
+    }
+  }
 
   return true;
 }
