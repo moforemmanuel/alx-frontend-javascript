f2e9d8546ffddf82cd3c85f30b0ce2351e6a49d6
= <=>
2022-04-12 23:42:45 +0100

    update map mod

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,11 +1,19 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) throw new Error('Cannot process');
 
-  map.forEach((key, value) => {
+  // map.forEach((key, value) => {
+  //   if (value === 1) {
+  //     map.set(key, 100);
+  //   }
+  // });
+
+  // return map;
+
+  for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);
     }
-  });
+  }
 
   return map;
 }
