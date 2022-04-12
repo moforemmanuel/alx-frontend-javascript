dd76235dfc115cca22da22e0dec4cd5e37923a27
= <=>
2022-04-12 21:49:16 +0100

    typed arr

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = ArrayBuffer(length);
+  const buffer = new ArrayBuffer(length);
   const int8view = new Int8Array(buffer);
 
   if (position > length - 1) {
