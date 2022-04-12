9bbaa898a8f06e2abeeabc445d346ebaa77e2c65
= <=>
2022-04-12 17:48:23 +0100

    ids sum

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,7 @@
 export default function getStudentIdsSum(students) {
   let idsSum;
   if (Array.isArray(students)) {
-    idsSum = students.reduce((acc, student) => acc + student.id);
+    idsSum = students.reduce((acc, student) => acc + student.id, 0);
   } else {
     idsSum = 0;
   }
