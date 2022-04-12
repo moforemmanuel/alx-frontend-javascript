968c0e8a4f53790922dedc65e436a1f21022465e
= <=>
2022-04-12 08:42:07 +0100

    final user

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,22 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName)]).catch((error) => {
+      return error;
+    }).then(values => {
+      let status;
+      const responses = [];
+      values.forEach(value => {
+        status = value instanceof Error ? 'failed' : 'fulfilled';
+        responses.push({
+          status,
+          value,
+        });
+      });
+
+      return responses;
+    });
+}
