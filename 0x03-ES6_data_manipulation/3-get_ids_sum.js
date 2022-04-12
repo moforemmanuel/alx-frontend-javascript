bc66d231780634547661037c6453e64b8adb302d
= <=>
2022-04-12 17:42:54 +0100

    ids sum

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,9 +1,7 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-return-assign */
 export default function getStudentIdsSum(students) {
   let idsSum;
   if (Array.isArray(students)) {
-    idsSum = students.reduce((acc, student) => acc += student.id);
+    idsSum = students.reduce((acc, student) => acc + student.id);
   } else {
     idsSum = 0;
   }
