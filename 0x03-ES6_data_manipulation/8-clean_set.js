a20960270e6758a9dfe93c1aeacd6119c253cfac
= <=>
2022-04-12 23:03:04 +0100

    startswith mod beta

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,25 @@
+/* eslint-disable operator-linebreak */
 export default function cleanSet(set, startString) {
-  return Array.from(set)
-    .filter((entry) => entry.startsWith(startString))
-    .map((item) => item.replace(startString, ''))
-    .join('-');
+  // return Array.from(set)
+  //   .filter((entry) => entry.startsWith(startString))
+  //   .map((item) => item.replace(startString, ''))
+  //   .join('-');
+
+  const str = [];
+
+  if (
+    typeof set !== 'object' ||
+    typeof startString !== 'string' ||
+    startString.length === 0
+  ) {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      str.push(item.slice(startString.length));
+    }
+  }
+
+  return str.join('-');
 }
